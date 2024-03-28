268cb00eb8f7d174a86344f06557854f6a4144bf
knwin <49636490+knwins@users.noreply.github.com>
2024-03-29 00:14:43 +0800

    Update thrend.tsx

--- src/pages/Coin/thrend.tsx
+++ src/pages/Coin/thrend.tsx
@@ -39,45 +39,13 @@ const Thrend: React.FC = () => {
       title: <FormattedMessage id="pages.coin.thrend.symbolType" />,
       dataIndex: 'symbolType',
       hideInSearch: true,
-      valueType: 'select',
-      valueEnum: {
-        FUTURE: {
-          symbolType: 'FUTURE',
-        },
-        SPOT: {
-          symbolType: 'SPOT',
-        },
-      },
+      valueType: 'text',
     },
 
     {
       title: <FormattedMessage id="pages.coin.thrend.threndType" />,
       dataIndex: 'threndType',
       valueType: 'select',
-       valueEnum: {
-        X1_LONG: {
-          threndType: 'X1_LONG',
-        },
-         X1_SHORT: {
-          threndType: 'X1_SHORT',
-        },
-         X2_LONG: {
-          text: 'X2_LONG',
-          threndType: 'X2_LONG',
-        },
-         X2_SHORT: {
-          text: 'X2_SHORT',
-          threndType: 'X2_SHORT',
-        },
-           X3_LONG: {
-          text: 'X3_LONG',
-          threndType: 'X3_LONG',
-        },
-         X3_SHORT: {
-          text: 'X3_SHORT',
-          threndType: 'X3_SHORT',
-        },
-      },
     },
 
     {
